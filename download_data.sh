750a16f3f57d47817d0441622b441ebc2b51dce8
luke-scot <lshc3@cam.ac.uk>
2021-04-07 18:18:08 +0000

    Geopal data plotted

--- download_data.sh
+++ download_data.sh
@@ -7,5 +7,8 @@ pip install gdown
 mkdir data
 cd data
 
+# OSM Beirut data download
+gdown -O beirutBuildingFootprints.geojson "https://drive.google.com/uc?export=download&id=1APHknp072orpaP8YFaLOP4VQisbuRcys"
+
 # GeoPal data download
 gdown -O geopalData.csv "https://drive.google.com/uc?export=download&id=1oTFgpPhkPMJzYfhHgAiTcEMYLqAg6TUe"
