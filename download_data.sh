788be6371f0974d28e3bbe2f82b8ed44510a0303
luke-scot <lshc3@cam.ac.uk>
2021-05-14 14:36:55 +0000

    Reinstate data download

--- download_data.sh
+++ download_data.sh
@@ -7,18 +7,18 @@ pip install gdown
 mkdir data
 cd data
 
-# # OSM Beirut data download
-# gdown -O beirutBuildingFootprints.geojson "https://drive.google.com/uc?export=download&id=1APHknp072orpaP8YFaLOP4VQisbuRcys"
+# OSM Beirut data download
+gdown -O beirutBuildingFootprints.geojson "https://drive.google.com/uc?export=download&id=1APHknp072orpaP8YFaLOP4VQisbuRcys"
 
-# # GeoPal data download
-# gdown -O geopalData.csv "https://drive.google.com/uc?export=download&id=1oTFgpPhkPMJzYfhHgAiTcEMYLqAg6TUe"
+# GeoPal data download
+gdown -O geopalData.csv "https://drive.google.com/uc?export=download&id=1oTFgpPhkPMJzYfhHgAiTcEMYLqAg6TUe"
 
-# # Beirut Interferometry data
-# # Pre Explosion
-# gdown -O beirutPrePreExplosionIfg.tif "https://drive.google.com/uc?export=download&id=1diYZtQJrQLZ1s8WMSvEGnY4wx-UT6vC7"
+# Beirut Interferometry data
+# Pre Explosion
+gdown -O beirutPrePreExplosionIfg.tif "https://drive.google.com/uc?export=download&id=1diYZtQJrQLZ1s8WMSvEGnY4wx-UT6vC7"
 
-# # Post Explosion
-# gdown -O beirutPrePostExplosionIfg.tif "https://drive.google.com/uc?export=download&id=1S_WYEJ6ikL6wskvoO7G3xGh-Lwb7EqlF"
+# Post Explosion
+gdown -O beirutPrePostExplosionIfg.tif "https://drive.google.com/uc?export=download&id=1S_WYEJ6ikL6wskvoO7G3xGh-Lwb7EqlF"
 
 # Bourj Hammoud Data
 gdown -O bourjHammoud.csv "https://drive.google.com/uc?export=download&id=1t_xpbzcO2svWJnO8FLd0yS3bvNFqu0Jo"
